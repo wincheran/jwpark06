be0002dd1053368975e53bdc5df8fcdac7cf012d
jaewon <wincheran@gmail.com>
2018-12-12 00:05:35 +0900

    테스트 케이스 작성 및 POJO 추가

---
+++ src/main/java/com/demo/pojo/Fruit.java
@@ -0,0 +1,9 @@
+package com.demo.pojo;
+
+import lombok.Data;
+
+@Data
+public class Fruit {
+    private String name;
+    private String color;
+}
---
+++ src/main/java/com/demo/pojo/FruitProperty.java
@@ -0,0 +1,14 @@
+package com.demo.pojo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "fruit")
+public class FruitProperty {
+    private List<Fruit> list;
+}
